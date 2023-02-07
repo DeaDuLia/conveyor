bb0cb7d55d0ae586c50a8fb7e05f2db71cc81a5a
DeaDuLia <50991329+DeaDuLia@users.noreply.github.com>
2023-02-07 11:00:31 +0300

    split mvps

---
+++ conveyor/src/main/java/com/application/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.application.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY
+}
---
+++ conveyor/src/main/java/com/application/enums/MaritalStatus.java
@@ -0,0 +1,8 @@
+package com.application.enums;
+
+public enum MaritalStatus {
+    MARRIED,
+    DIVORCED,
+    SINGLE,
+    WIDOW_WIDOWER
+}
