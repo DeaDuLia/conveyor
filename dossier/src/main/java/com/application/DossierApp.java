8d3614bcb190ec82d4716b636b5aa58aa14aaf9b
DeaDuLia <50991329+DeaDuLia@users.noreply.github.com>
2023-02-09 13:50:36 +0300

    good email sending

---
+++ dossier/src/main/java/com/application/DossierApp.java
@@ -0,0 +1,61 @@
+package com.application;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
+@SpringBootApplication
+@EnableFeignClients
+public class DossierApp {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DossierApp.class, args);
+	}
+
+	@Bean
+	public NewTopic topic1() {
+		return TopicBuilder.name("finish-registration")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+	@Bean
+	public NewTopic topic2() {
+		return TopicBuilder.name("create-documents")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+	@Bean
+	public NewTopic topic3() {
+		return TopicBuilder.name("send-documents")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+	@Bean
+	public NewTopic topic4() {
+		return TopicBuilder.name("send-ses")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+	@Bean
+	public NewTopic topic5() {
+		return TopicBuilder.name("credit-issued")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+	@Bean
+	public NewTopic topic6() {
+		return TopicBuilder.name("application-denied")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+}
--- dossier/src/main/java/com/application/service/EmailMessageService.java
+++ dossier/src/main/java/com/application/service/EmailMessageService.java
@@ -2,38 +2,50 @@ package com.application.service;
 
 import com.application.dto.MsgDTO;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
 
 @Slf4j
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EmailMessageService {
-    private final JavaMailSender javaMailSender;
+    @Value("${mail.user}")
+    private String user;
+    @Value("${mail.pass}")
+    private String password;
+    @Value("${mail.smtp.host}")
+    private String hostSMTP;
+    @Value("${mail.smtp.port}")
+    private Integer port;
+    @Value("${mail.smtp.ssl.enable}")
+    private String ssl_enable;
+    @Value("${mail.smtp.auth}")
+    private String auth;
 
     public void sendEmail(String email, String theme) {
         log.info("Create Msg");
         MsgDTO msgDTO = createEmail(email, theme);
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(msgDTO.getAddress());
-        msg.setSubject(msgDTO.getTitle());
-        msg.setText(msgDTO.getBody());
         try {
-            log.info("send" + theme + " msg to " + email);
-            javaMailSender.send(msg);
+            log.info("send '" + theme + "' msg to " + email);
+            sendmail(msgDTO);
             log.info("sending completed");
         } catch (Exception e) {
-            log.warn("\n" +
-                    "Email should be sent here, but Gmail is bad :s" +
-                    "\nBut the method itself works, honestly!" +
-                    "\nhere is the error message from gmail.com service: \n" + e.getMessage());
+            log.warn("Can't send message: \n" + e.getMessage());
         }
     }
 
     public MsgDTO createEmail (String address, String theme) {
         String body = "Default body text";
+        //Для простого сообщения можно было бы использовать String body = "Text for " + theme;
+        //Но оператор switch используется на случай, если текст сообщения будет более осоззанным и сложным
         switch (theme) {
             case "finish-registration":
                 body = "Text for finish-registration";
@@ -55,5 +67,29 @@ public class EmailMessageService {
                 break;
         }
         return new MsgDTO(address, theme, body);
+    }
+
+    private void sendmail(MsgDTO msgDTO) throws MessagingException, IOException {
+        String to = msgDTO.getAddress();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", hostSMTP);
+        props.put("mail.smtp.ssl.enable", ssl_enable);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", auth);
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(user));
+        InternetAddress[] addresses = {new InternetAddress(to)};
+        msg.setRecipients(Message.RecipientType.TO, addresses);
+        msg.setSubject(msgDTO.getTitle());
+        msg.setSentDate(new Date());
+        msg.setText(msgDTO.getBody());
+        Transport.send(msg);
     }
 }
